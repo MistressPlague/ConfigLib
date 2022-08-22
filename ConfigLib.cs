2f023f926c4464a29b50ec0fc4459918dcc791cc
Mistress Plague <36628963+MistressPlague@users.noreply.github.com>
2022-08-22 11:25:36 +0100

    Update ConfigLib.cs

--- ConfigLib.cs
+++ ConfigLib.cs
@@ -1,15 +1,13 @@
 using System;
-using System.Collections.Generic;
 using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using Newtonsoft.Json;
 
 namespace Libraries
 {
     public class ConfigLib<T> where T : class
     {
+        private string ConfigCache;
+
         public ConfigLib(string configPath)
         {
             ConfigPath = configPath;
@@ -29,20 +27,20 @@ namespace Libraries
 
             if (!File.Exists(ConfigPath))
             {
-                File.WriteAllText(ConfigPath, JsonConvert.SerializeObject(Activator.CreateInstance(typeof(T)), Newtonsoft.Json.Formatting.Indented));
+                File.WriteAllText(ConfigPath, JsonConvert.SerializeObject(Activator.CreateInstance(typeof(T)), Formatting.Indented));
             }
 
             InternalConfig = JsonConvert.DeserializeObject<T>(File.ReadAllText(ConfigPath));
 
             var timer = new System.Timers.Timer(1000);
 
-            var ConfigCache = JsonConvert.SerializeObject(InternalConfig);
+            ConfigCache = JsonConvert.SerializeObject(InternalConfig);
 
-            timer.Elapsed += (sender, args) =>
+            timer.Elapsed += (_, _) =>
             {
-                if (JsonConvert.SerializeObject(InternalConfig) != ConfigCache)
+                if (JsonConvert.SerializeObject(InternalConfig, Formatting.Indented) != ConfigCache)
                 {
-                    ConfigCache = JsonConvert.SerializeObject(InternalConfig);
+                    ConfigCache = JsonConvert.SerializeObject(InternalConfig, Formatting.Indented);
 
                     SaveConfig();
 
@@ -70,22 +68,13 @@ namespace Libraries
         {
             try
             {
-                var UpdatedConfig = JsonConvert.DeserializeObject<T>(File.ReadAllText(ConfigPath));
+                var ConfigData = File.ReadAllText(ConfigPath);
 
-                if (UpdatedConfig != null)
+                if (ConfigCache != ConfigData)
                 {
-                    foreach (var newProp in UpdatedConfig.GetType()?.GetProperties())
-                    {
-                        var OldProp = InternalConfig.GetType().GetProperty(newProp?.Name);
+                    InternalConfig = JsonConvert.DeserializeObject<T>(ConfigData);
 
-                        if (OldProp != null && newProp.GetValue(UpdatedConfig) != OldProp.GetValue(InternalConfig)) // Property Existed Before & Has Changed
-                        {
-                            InternalConfig = UpdatedConfig;
-
-                            OnConfigUpdated?.Invoke();
-                            break;
-                        }
-                    }
+                    OnConfigUpdated?.Invoke();
                 }
             }
             catch
@@ -95,23 +84,7 @@ namespace Libraries
 
         public void SaveConfig()
         {
-            File.WriteAllText(ConfigPath, JsonConvert.SerializeObject(InternalConfig, Newtonsoft.Json.Formatting.Indented));
-        }
-    }
-
-    public static class ConfigExt
-    {
-        public static bool DoesInstanceMatch<T>(this T instance, T CompareTo) where T : class
-        {
-            foreach (var prop in instance.GetType().GetProperties())
-            {
-                if (JsonConvert.SerializeObject(prop.GetValue(instance)) != JsonConvert.SerializeObject(prop.GetValue(CompareTo)))
-                {
-                    return false;
-                }
-            }
-
-            return true;
+            File.WriteAllText(ConfigPath, JsonConvert.SerializeObject(InternalConfig,  Formatting.Indented));
         }
     }
 }
